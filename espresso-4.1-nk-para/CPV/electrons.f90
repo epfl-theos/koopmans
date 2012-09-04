b09441c354bebaa441c51e5dccc38f317518f371
gborghi <>
2012-09-04 14:09:20 +0000

    inserted some flags to print HOMO and LUMO in electrons.f90

--- espresso-4.1-nk-para/CPV/electrons.f90
+++ espresso-4.1-nk-para/CPV/electrons.f90
@@ -267,20 +267,24 @@
       !
       ik = 1
       !
-      IF(nspin==1) THEN
-         WRITE( stdout,1101) 
-         WRITE( stdout, 1444) MAXVAL(ei(:,1)*autoev, nupdwn(1))
-      ELSE
-         WRITE( stdout,1101) 
-         WRITE( stdout, 1444) MAX(MAXVAL(ei(:,1)*autoev, nupdwn(1)), MAXVAL(ei(:,2)*autoev, nupdwn(2)))
-      ENDIF
- 
-      IF(nspin==1) THEN
-         WRITE( stdout,1201) 
-         WRITE( stdout, 1444) MAXVAL(ei_emp(:,1)*autoev, nupdwn(1))
-      ELSE
-         WRITE( stdout,1201) 
-         WRITE( stdout, 1444) MAX(MAXVAL(ei_emp(:,1)*autoev, nupdwn(1)), MAXVAL(ei_emp(:,2)*autoev, nupdwn(2)))
+      IF(tstdout) THEN
+         IF(nspin==1) THEN
+            WRITE( stdout,1101) 
+            WRITE( stdout, 1444) MAXVAL(ei(:,1)*autoev, nupdwn(1))
+         ELSE
+            WRITE( stdout,1101) 
+            WRITE( stdout, 1444) MAX(MAXVAL(ei(:,1)*autoev, nupdwn(1)), MAXVAL(ei(:,2)*autoev, nupdwn(2)))
+         ENDIF
+      
+         IF(n_emp.gt.0) THEN 
+            IF(nspin==1) THEN
+               WRITE( stdout,1201) 
+               WRITE( stdout, 1444) MAXVAL(ei_emp(:,1)*autoev, nupdwn(1))
+            ELSE
+               WRITE( stdout,1201) 
+               WRITE( stdout, 1444) MAX(MAXVAL(ei_emp(:,1)*autoev, nupdwn(1)), MAXVAL(ei_emp(:,2)*autoev, nupdwn(2)))
+            ENDIF
+         ENDIF
       ENDIF
 
       DO j = 1, nspin
