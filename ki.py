7f46442e56c177854c593f16ff78d7c001bf3b63
Edward Linscott <edward.linscott@gmail.com>
2020-01-27 11:16:54 +0100

    Turned on copying of KS orbitals from PBE CP as a better starting point than the arbitrary CP orbitals.

--- ki.py
+++ ki.py
@@ -63,9 +63,10 @@ def run_ki(master_cpi, alpha_guess=0.6, alpha_from_file=False, n_max_sc_steps=1,
     calc.directory = 'init'
     run_cp(calc, silent=False, from_scratch=from_scratch)
 
-    # # Moving orbitals
-    # os.system('cp TMP-CP/pbe_50.save/K00001/evc1.dat TMP-CP/pbe_50.save/K00001/evc01.dat')
-    # os.system('cp TMP-CP/pbe_50.save/K00001/evc2.dat TMP-CP/pbe_50.save/K00001/evc02.dat')
+    # Moving orbitals
+    savedir = f'{calc.directory}/TMP-CP/pbe_50.save/K00001'
+    os.system(f'cp {savedir}/evc1.dat {savedir}/evc01.dat')
+    os.system(f'cp {savedir}/evc2.dat {savedir}/evc02.dat')
 
     # PZ reading in PBE to define manifold
     if alpha_from_file:
