ea200cf67b58bcae40933d9f68baaaddba256742
Edward Linscott <edward.linscott@gmail.com>
2020-02-06 09:51:55 +0100

    Updated koopmans.py to reflect json modifications

--- koopmans.py
+++ koopmans.py
@@ -10,20 +10,9 @@ if __name__ == '__main__':
 
     parser = argparse.ArgumentParser(
         description='Perform a KI/KIPZ calculation using cp.x')
-    parser.add_argument('orbdep', metavar='orbdep', type=str,
-                        help="choice of orbital-dependent functional; must be one of 'ki'/'kipz'")
     parser.add_argument('json', metavar='system.json', type=str,
-                        help='a JSON file containing the atomic positions, number of atoms etc.')
-    parser.add_argument('-a', '--alpha', default=0.6, type=float,
-                        help='starting guess for alpha as a single float')
-    parser.add_argument('-c', '--cont', action='store_true',
-                        help='continue from a previous calculation')
-    parser.add_argument('-f', '--alpha_from_file', action='store_true',
-                        help='read in starting guess for alpha from file')
-    parser.add_argument('-i', '--maxit', default=1, type=int,
-                        help='maximum number of self-consistent iterations')
+                        help='a single JSON file containing the workflow and cp.x settings')
 
     args = parser.parse_args()
 
-    workflow.run(args.orbdep, args.json, args.alpha,
-                 args.alpha_from_file, args.maxit, not args.cont)
+    workflow.run(args.json)
