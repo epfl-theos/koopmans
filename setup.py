d67f4de86549e3d6c4ca8c713e4f9219aa0dae1c
Edward Linscott <edwardlinscott@gmail.com>
2021-10-08 14:48:09 +0200

    Increment python version

--- setup.py
+++ setup.py
@@ -27,7 +27,7 @@ with open("README.rst", "r") as f:
     long_description = f.read()
 
 setup(name='koopmans',
-      version='0.3.2',
+      version='0.4.0',
       description='Koopmans spectral functional calculations with python and Quantum ESPRESSO',
       long_description=long_description,
       url='https://github.com/epfl-theos/koopmans',
