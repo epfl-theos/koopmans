d7558f741246e672e435ec2d1d2c792562df160e
degennar <riccardo.degennar@gmail.com>
2021-12-03 12:01:33 +0100

    bug fix

--- koopmans/calculators/_ui/_calculator.py
+++ koopmans/calculators/_ui/_calculator.py
@@ -270,7 +270,7 @@ class UnfoldAndInterpolateCalculator(CalculatorExt, Calculator, CalculatorABC):
             if single_R:
                 assert int(lines[1].split()[0]) == self.parameters.num_wann_sc, 'In parse_hr inconsistency in num_wann'
             else:
-                assert int(lines[1].split()[0]) != self.parameters.num_wann, 'In parse_hr inconsistency in num_wann'
+                assert int(lines[1].split()[0]) == self.parameters.num_wann, 'In parse_hr inconsistency in num_wann'
 
             if not single_R:
                 rvect = []
