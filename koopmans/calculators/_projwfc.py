e63a2a8aa748a041a5fe41cf7505561911985899
Edward Linscott <edwardlinscott@gmail.com>
2021-12-22 18:30:09 +0100

    Added framework for parsing of pdos files generated by projwfc

--- koopmans/calculators/_projwfc.py
+++ koopmans/calculators/_projwfc.py
@@ -9,6 +9,8 @@ from ase import Atoms
 from koopmans.commands import Command, ParallelCommand
 from koopmans.settings import ProjwfcSettingsDict
 from ase.calculators.espresso import Projwfc
+from ase.spectrum.dosdata import GridDOSData
+from ase.spectrum.doscollection import GridDOSCollection
 from ._utils import CalculatorExt, CalculatorABC, qe_bin_directory
 
 
@@ -31,7 +33,32 @@ class ProjwfcCalculator(CalculatorExt, Projwfc, CalculatorABC):
                 'ASE_PROJWFC_COMMAND', str(qe_bin_directory) + os.path.sep + self.command))
 
         self.results_for_qc = []
+
+    def calculate(self):
+        super().calculate()
+
+        # Marija add code here to
+        # 1) work out the names of the pDOS files to read
+        #
+        # 2) read in the pDOS file contents
+        # dos = self.read_pdos(filename)
+        #
+        # 3) add pDOS to self.results
+        # self.results['dos'] = GridDOSCollection(...)
+
+    def read_pdos(filename: str) -> GridDOSData:
+        # Marija: implement in this function how to extract from a DOS filename the contents of that file
+        with open(filename, 'r') as fd:
+            flines = fd.readlines()
+
+        # Logic here to turn flines into a DOS object
+        # dos = GridDOSData(...)
+
+        return dos
+
     def is_complete(self):
+        # Change me to check calc.results['job done']!
         return True
+
     def is_converged(self):
         return True
