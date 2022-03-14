157b272a7a1af970e6c08b26c8a94428f1a6b7bd
degennar <riccardo.degennar@gmail.com>
2022-03-14 23:45:50 +0100

    adding ase and qe_koopmans develop commits

--- koopmans/settings/_wann2kcp.py
+++ koopmans/settings/_wann2kcp.py
@@ -4,7 +4,7 @@ from ._utils import SettingsDict
 class Wann2KCPSettingsDict(SettingsDict):
     def __init__(self, **kwargs) -> None:
         super().__init__(valid=['outdir', 'prefix', 'seedname', 'wan_mode', 'spin_component',
-                                'gamma_trick', 'print_rho', 'wannier_plot', 'wannier_plot_list']
+                                'gamma_trick', 'print_rho', 'wannier_plot', 'wannier_plot_list'],
                          defaults={'outdir': 'TMP', 'prefix': 'kc', 'seedname': 'wann',
                                    'wan_mode': 'wannier2kcp'},
                          are_paths=['outdir'],
