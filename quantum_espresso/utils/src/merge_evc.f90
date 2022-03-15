07d65eca277b497616aa13c570e391ad42202c07
degennar <riccardo.degennar@gmail.com>
2022-03-15 20:29:55 +0100

    making merge_evc.x totally independent

--- quantum_espresso/utils/src/merge_evc.f90
+++ quantum_espresso/utils/src/merge_evc.f90
@@ -10,13 +10,149 @@
 !
 !
 !---------------------------------------------------------------------
+MODULE merge_evc_module
+  !-----------------------------------------------------------------
+  !
+  IMPLICIT NONE
+  !
+  SAVE
+  !
+  PRIVATE
+  !
+  INTEGER, PARAMETER, PUBLIC :: stdout = 6
+  INTEGER, PARAMETER, PUBLIC :: DP = selected_real_kind(14,200)
+  !
+  PUBLIC :: parse_args, error_stop
+  !
+  CONTAINS
+  !
+  !---------------------------------------------------------------------
+  SUBROUTINE parse_args( io_files, nrtot, output_exst )
+      !-----------------------------------------------------------------
+      !
+      ! ... routine parsing arguments passed in input
+      !
+      !
+      IMPLICIT NONE
+      !
+      CHARACTER(LEN=256), ALLOCATABLE, INTENT(OUT) :: io_files(:)
+      INTEGER, INTENT(OUT) :: nrtot
+      LOGICAL, INTENT(OUT) :: output_exst
+      !
+      INTEGER :: nargs, iarg, n_files
+      INTEGER :: counter, ios
+      CHARACTER(LEN=256) :: arg
+      LOGICAL :: input_exst = .false.
+      LOGICAL :: nrtot_exst = .false.
+      !
+      !
+      nargs = command_argument_count()
+      !   
+      IF ( nargs < 2 .or. mod( nargs, 2 ) /= 0 ) &
+        CALL error_stop( 'wrong number of positional arguments', 1 )
+      !
+      output_exst = .false.
+      n_files = nargs / 2 - 1
+      ALLOCATE( io_files(n_files) )
+      io_files(:) = ' '
+      iarg = 1
+      counter = 0
+      !
+      DO WHILE ( iarg < nargs )
+        !
+        CALL get_command_argument( iarg, arg )
+        iarg = iarg + 1
+        !
+        SELECT CASE ( trim(arg) )
+        !
+        CASE ( '-nr' )
+          CALL get_command_argument( iarg, arg )
+          READ( arg, *, IOSTAT=ios )  nrtot
+          IF ( ios .ne. 0 ) &
+            CALL error_stop( 'error while reading number of R-vectors', abs(ios) )
+          IF ( nrtot_exst ) &
+            CALL error_stop( 'nrtot can be defined only once', 1 )
+          nrtot_exst = .true.
+        !
+        CASE ( '-i', '-in', '-input' )
+          CALL get_command_argument( iarg, arg )
+          counter = counter + 1
+          IF ( counter > n_files ) &
+            CALL error_stop( 'something wrong in input. Did you provide -nr ?', 1 )
+          io_files(counter) = trim(arg)
+          IF ( .not. input_exst ) input_exst = .true.
+        !
+        CASE ( '-o', '-out', '-output' )
+          CALL get_command_argument( iarg, arg )
+          IF ( output_exst ) &
+            CALL error_stop( 'only one output file can be defined', 1 )
+          output_exst = .true.
+          io_files(n_files) = trim(arg)
+        !
+        CASE DEFAULT
+          CALL error_stop( 'unrecognised argument option', abs(iarg-1) )
+        !
+        END SELECT
+        !
+        iarg = iarg + 1
+        !
+      ENDDO
+      !
+      IF ( .not. nrtot_exst ) CALL error_stop( 'nrtot was not provided', 1)
+      !
+      IF ( .not. input_exst ) CALL error_stop( 'no input files provided', 1 )
+      IF ( nrtot .le. 0 ) CALL error_stop( 'wrong number of R-vectors', abs(nrtot) )
+      !
+      !
+  END SUBROUTINE parse_args
+  !
+  !!---------------------------------------------------------------------
+  SUBROUTINE error_stop( message, ierr )
+    !-----------------------------------------------------------------
+    !
+    ! ... error handler
+    !
+    !
+    IMPLICIT NONE
+    !
+    CHARACTER(LEN=*), INTENT(IN) :: message
+    INTEGER, INTENT(IN) :: ierr
+    !
+    LOGICAL :: opnd
+    !
+    !
+    IF ( ierr <= 0 ) RETURN
+    !
+    ! ... the error message is written un the "*" unit
+    !
+    WRITE (UNIT=*, FMT='(/,1X,78("%"))')
+    WRITE (UNIT=*, FMT='(5X,"from ",A," : error #",I10)') 'merge_evc', ierr
+    WRITE (UNIT=*, FMT='(5X,A)') message
+    WRITE (UNIT=*, FMT='(1X,78("%"),/)')
+    WRITE (*, '("     stopping ...")')
+    !
+    INQUIRE( UNIT = stdout, OPENED = opnd )
+    !
+    IF ( opnd ) CALL flush( stdout )
+    !
+    ERROR STOP 1
+    !
+    RETURN
+    !
+  END SUBROUTINE error_stop
+  !
+  !
+END MODULE merge_evc_module
+!
+!
+!---------------------------------------------------------------------
 PROGRAM merge_evc
     !-----------------------------------------------------------------
     !
     ! This program merges all the wavefunction files passed as
     ! standard input arguments. The program can be run as below: 
     ! 
-    ! merge_evc.x -i path/to/file1 -i path/to/file2 -o output
+    ! merge_evc.x -nr 4 -i path/to/file1 -i path/to/file2 -o output
     ! 
     ! The -nr argument is mandatory and it is followed by an integer
     ! specifying the number of R-vectors, i.e. the number of
@@ -29,10 +165,6 @@ PROGRAM merge_evc
     ! the output file; if not present the default output name is
     ! evcw.dat
     !
-    ! By adding -v or --verbose as a first argument, the standard
-    ! output of a QE program is printed out.
-    ! No output is printed by default.
-    !
     ! For the moment the format of the files is that defined in
     ! cp_files.f90, used by the Koopmans-CP code.
     !
@@ -40,18 +172,13 @@ PROGRAM merge_evc
     ! parallelization
     !
     !
-    USE io_global,              ONLY : stdout, ionode
-    USE mp_global,              ONLY : mp_startup
-    USE environment,            ONLY : environment_start, environment_end
-    USE kinds,                  ONLY : DP
-    !
+    USE merge_evc_module
     !
     IMPLICIT NONE
     !
     CHARACTER(LEN=20) :: arg
     CHARACTER(LEN=256), ALLOCATABLE :: io_files(:)
     CHARACTER(LEN=256) :: filename, ofilename, nfile
-    LOGICAL :: verbosity = .false.
     LOGICAL :: output_exst
     INTEGER :: ifile, ios, n_files
     INTEGER :: iunit = 8
@@ -59,36 +186,14 @@ PROGRAM merge_evc
     INTEGER :: iunit_
     INTEGER :: npw, npw_ref
     INTEGER :: ipw, ibnd, ir
-    INTEGER :: nbnd, nrtot, istart
+    INTEGER :: nbnd, nrtot
     INTEGER, ALLOCATABLE :: nbnd_i(:)
     COMPLEX(DP), ALLOCATABLE :: evc(:)
     !
-    INTERFACE
-      SUBROUTINE parse_args( istart, io_files, nrtot, output_exst )
-        INTEGER, INTENT(IN) :: istart
-        CHARACTER(LEN=256), ALLOCATABLE, INTENT(OUT) :: io_files(:)
-        INTEGER, INTENT(OUT) :: nrtot
-        LOGICAL, INTENT(OUT) :: output_exst
-      END SUBROUTINE
-    END INTERFACE
-    !
-    !
-#if defined(__MPI)
-    CALL mp_startup( )
-#endif
     !
     CALL get_command_argument( 1, arg )
-    IF ( trim(arg) == '-v' .or. trim(arg) == '--verbose' ) verbosity = .true.
     !
-    IF ( verbosity ) THEN
-      CALL environment_start( 'MERGE_EVC' )
-      WRITE( stdout, * )
-      istart = 2
-    ELSE
-      istart = 1
-    ENDIF
-    !
-    CALL parse_args( istart, io_files, nrtot, output_exst )
+    CALL parse_args( io_files, nrtot, output_exst )
     !
     IF ( output_exst ) THEN
       ofilename = trim( io_files(size(io_files)) )
@@ -98,171 +203,72 @@ PROGRAM merge_evc
       n_files = size( io_files )
     ENDIF
     !
-    IF ( ionode ) THEN
+    ! ... reading number of PWs and number of bands from each file
+    !
+    nbnd = 0
+    ALLOCATE( nbnd_i(n_files) )
+    !
+    DO ifile = 1, n_files
       !
-      ! ... reading number of PWs and number of bands from each file
+      iunit_ = iunit + ifile
+      filename = trim(io_files(ifile))
+      OPEN( UNIT=iunit_, FILE=trim(filename), STATUS='old', FORM='unformatted', IOSTAT=ios )
       !
-      nbnd = 0
-      ALLOCATE( nbnd_i(n_files) )
+      IF ( ios .ne. 0 ) THEN
+        CLOSE( UNIT=ounit, STATUS='delete' )
+        CALL error_stop( 'problem opening the input files', ifile )
+      ENDIF
+      !
+      READ( iunit_ ) npw, nbnd_i(ifile)
+      !
+      IF ( mod( nbnd_i(ifile), nrtot ) .ne. 0 ) &
+        CALL error_stop( 'incosistent number of R-vectors', mod( nbnd_i(ifile), nrtot ) )
+      !
+      IF ( ifile == 1 ) npw_ref = npw
+      !
+      IF ( npw .ne. npw_ref ) THEN
+        CLOSE( UNIT=ounit, STATUS='delete' )
+        CALL error_stop( 'unconsistent number of PW between files', ifile )
+      ENDIF
+      !
+    ENDDO
+    !
+    nbnd = SUM( nbnd_i )
+    OPEN( UNIT=ounit, FILE=trim(ofilename), STATUS='unknown', FORM='unformatted' )
+    WRITE( ounit ) npw, nbnd
+    ALLOCATE( evc(npw) )
+    !
+    ! ... reading wave functions and writing them to a single file
+    !
+    DO ir = 1, nrtot
       !
       DO ifile = 1, n_files
         !
         iunit_ = iunit + ifile
-        filename = trim(io_files(ifile))
-        OPEN( UNIT=iunit_, FILE=trim(filename), STATUS='old', FORM='unformatted', IOSTAT=ios )
         !
-        IF ( ios .ne. 0 ) THEN
-          CLOSE( UNIT=ounit, STATUS='delete' )
-          CALL errore( 'merge_evc', 'problem opening the input files', ifile )
-        ENDIF
+        IF ( ir == 1 ) WRITE( nfile, 100 ) ifile
         !
-        READ( iunit_ ) npw, nbnd_i(ifile)
-        !
-        IF ( mod( nbnd_i(ifile), nrtot ) .ne. 0 ) &
-          CALL errore( 'merge_evc', 'incosistent number of R-vectors', mod( nbnd_i(ifile), nrtot ) )
-        !
-        IF ( ifile == 1 ) npw_ref = npw
-        !
-        IF ( npw .ne. npw_ref ) THEN
-          CLOSE( UNIT=ounit, STATUS='delete' )
-          CALL errore( 'merge_evc', 'unconsistent number of PW between files', ifile )
-        ENDIF
-        !
-      ENDDO
-      !
-      nbnd = SUM( nbnd_i )
-      OPEN( UNIT=ounit, FILE=trim(ofilename), STATUS='unknown', FORM='unformatted' )
-      WRITE( ounit ) npw, nbnd
-      ALLOCATE( evc(npw) )
-      !
-      ! ... reading wave functions and writing them to a single file
-      !
-      DO ir = 1, nrtot
-        !
-        DO ifile = 1, n_files
+        DO ibnd = 1, nbnd_i(ifile)/nrtot
           !
-          iunit_ = iunit + ifile
-          !
-          IF ( ir == 1 ) THEN
-            WRITE( nfile, 100 ) ifile
-            CALL start_clock( nfile )
-          ENDIF
-          !
-          DO ibnd = 1, nbnd_i(ifile)/nrtot
-            !
-            evc(:) = ( 0.d0, 0.d0 )
-            READ ( iunit_ ) ( evc(ipw), ipw = 1, npw )
-            WRITE( ounit ) ( evc(ipw), ipw = 1, npw )
-            !
-          ENDDO
-          !
-          IF ( ir == nrtot ) THEN
-            CLOSE( UNIT=iunit_ )
-            WRITE( nfile, 100 ) ifile
-            CALL stop_clock( nfile )
-            IF ( verbosity ) CALL print_clock( nfile )
-          ENDIF
+          evc(:) = ( 0.d0, 0.d0 )
+          READ ( iunit_ ) ( evc(ipw), ipw = 1, npw )
+          WRITE( ounit ) ( evc(ipw), ipw = 1, npw )
           !
         ENDDO
         !
+        IF ( ir == nrtot ) THEN
+          CLOSE( UNIT=iunit_ )
+          WRITE( nfile, 100 ) ifile
+        ENDIF
+        !
       ENDDO
       !
-      CLOSE( UNIT=ounit )
-      !
-    ENDIF
+    ENDDO
     !
-    !
-    IF ( verbosity ) THEN
-      WRITE( stdout, * )
-      WRITE( stdout, fmt='(5X,A,A)' ) 'Merged wave functions written into : ', trim(ofilename)
-      CALL environment_end( 'MERGE_EVC' )
-      WRITE( stdout, * )
-    ENDIF
+    CLOSE( UNIT=ounit )
     !
     !
 100 FORMAT( 'r/w file', 1X, I2 )
     !
     !
 END PROGRAM merge_evc
-!
-!
-!---------------------------------------------------------------------
-SUBROUTINE parse_args( istart, io_files, nrtot, output_exst )
-    !-----------------------------------------------------------------
-    !
-    ! ... routine parsing arguments passed in input
-    !
-    !
-    IMPLICIT NONE
-    !
-    INTEGER, INTENT(IN) :: istart
-    CHARACTER(LEN=256), ALLOCATABLE, INTENT(OUT) :: io_files(:)
-    INTEGER, INTENT(OUT) :: nrtot
-    LOGICAL, INTENT(OUT) :: output_exst
-    !
-    INTEGER :: nargs, iarg, n_files
-    INTEGER :: counter, ios
-    CHARACTER(LEN=256) :: arg
-    LOGICAL :: input_exst = .false.
-    LOGICAL :: nrtot_exst = .false.
-    !
-    !
-    nargs = command_argument_count()
-    !   
-    IF ( nargs - istart < 3 .or. mod( nargs-istart, 2 ) == 0 ) &
-      CALL errore( 'parse_args', 'wrong number of positional arguments', 1 )
-    !
-    output_exst = .false.
-    n_files = ( nargs - istart - 1 ) / 2
-    ALLOCATE( io_files(n_files) )
-    io_files(:) = ' '
-    iarg = istart
-    counter = 0
-    !
-    DO WHILE ( iarg < nargs )
-      !
-      CALL get_command_argument( iarg, arg )
-      iarg = iarg + 1
-      !
-      SELECT CASE ( trim(arg) )
-      !
-      CASE ( '-nr' )
-        CALL get_command_argument( iarg, arg )
-        READ( arg, *, IOSTAT=ios )  nrtot
-        IF ( ios .ne. 0 ) &
-          CALL errore( 'parse_args', 'error while reading number of R-vectors', abs(ios) )
-        IF ( nrtot_exst ) &
-          CALL errore( 'parse_args', 'nrtot can be defined only once', 1 )
-        nrtot_exst = .true.
-      !
-      CASE ( '-i', '-in', '-input' )
-        CALL get_command_argument( iarg, arg )
-        counter = counter + 1
-        IF ( counter > n_files ) &
-          CALL errore( 'parse_args', 'something wrong in input. Did you provide -nr ?', 1 )
-        io_files(counter) = trim(arg)
-        IF ( .not. input_exst ) input_exst = .true.
-      !
-      CASE ( '-o', '-out', '-output' )
-        CALL get_command_argument( iarg, arg )
-        IF ( output_exst ) &
-          CALL errore( 'parse_args', 'only one output file can be defined', 1 )
-        output_exst = .true.
-        io_files(n_files) = trim(arg)
-      !
-      CASE DEFAULT
-        CALL errore( 'parse_args', 'unrecognised argument option', abs(iarg-1) )
-      !
-      END SELECT
-      !
-      iarg = iarg + 1
-      !
-    ENDDO
-    !
-    IF ( .not. nrtot_exst ) CALL errore( 'parse_args', 'nrtot was not provided', 1)
-    !
-    IF ( .not. input_exst ) CALL errore( 'parse_args', 'no input files provided', 1 )
-    IF ( nrtot .le. 0 ) CALL errore( 'parse_args', 'wrong number of R-vectors', abs(nrtot) )
-    !
-    !
-END SUBROUTINE parse_args
