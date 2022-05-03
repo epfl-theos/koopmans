6eb5f53ad2bc3831f3a4729dbd68aee56e3c1ab6
Edward Linscott <edwardlinscott@gmail.com>
2022-05-03 17:28:57 +0200

    Giving the dft_bands workflow a subdirectory and a heading

--- koopmans/workflows/_dft.py
+++ koopmans/workflows/_dft.py
@@ -70,40 +70,43 @@ class PWBandStructureWorkflow(Workflow):
 
     def _run(self):
 
-        # First, a scf calculation
-        calc_scf = self.new_calculator('pw', nbnd=None)
-        calc_scf.prefix = 'scf'
-        self.run_calculator(calc_scf)
+        self.print('DFT bandstructure workflow', style='heading')
 
-        # Second, a bands calculation
-        calc_bands = self.new_calculator('pw', calculation='bands', kpts=self.kpath)
-        calc_bands.prefix = 'bands'
-        self.run_calculator(calc_bands)
+        with utils.chdir('dft_bands'):
+            # First, a scf calculation
+            calc_scf = self.new_calculator('pw', nbnd=None)
+            calc_scf.prefix = 'scf'
+            self.run_calculator(calc_scf)
 
-        # Prepare the band structure for plotting
-        bs = calc_bands.results['band structure']
-        n_filled = pseudopotentials.nelec_from_pseudos(self.atoms, self.pseudopotentials, self.pseudo_dir) // 2
-        vbe = bs._energies[:, :, :n_filled].max()
-        bs._energies -= vbe
+            # Second, a bands calculation
+            calc_bands = self.new_calculator('pw', calculation='bands', kpts=self.kpath)
+            calc_bands.prefix = 'bands'
+            self.run_calculator(calc_bands)
 
-        # Third, a PDOS calculation
-        pseudos = [pseudopotentials.read_pseudo_file(calc_scf.parameters.pseudo_dir / p) for p in
-                   self.pseudopotentials.values()]
-        if all([int(p.find('PP_HEADER').get('number_of_wfc')) > 0 for p in pseudos]):
-            calc_dos = self.new_calculator('projwfc', filpdos=self.name)
-            calc_dos.pseudopotentials = self.pseudopotentials
-            calc_dos.spin_polarised = self.parameters.spin_polarised
-            calc_dos.pseudo_dir = calc_bands.parameters.pseudo_dir
-            self.run_calculator(calc_dos)
+            # Prepare the band structure for plotting
+            bs = calc_bands.results['band structure']
+            n_filled = pseudopotentials.nelec_from_pseudos(self.atoms, self.pseudopotentials, self.pseudo_dir) // 2
+            vbe = bs._energies[:, :, :n_filled].max()
+            bs._energies -= vbe
 
-            # Prepare the DOS for plotting
-            dos = calc_dos.results['dos']
-            dos._energies -= vbe
-        else:
-            # Skip if the pseudos don't have the requisite PP_PSWFC blocks
-            utils.warn('Some of the pseudopotentials do not have PP_PSWFC blocks, which means a projected DOS '
-                       'calculation is not possible. Skipping...')
-            dos = None
+            # Third, a PDOS calculation
+            pseudos = [pseudopotentials.read_pseudo_file(calc_scf.parameters.pseudo_dir / p) for p in
+                       self.pseudopotentials.values()]
+            if all([int(p.find('PP_HEADER').get('number_of_wfc')) > 0 for p in pseudos]):
+                calc_dos = self.new_calculator('projwfc', filpdos=self.name)
+                calc_dos.pseudopotentials = self.pseudopotentials
+                calc_dos.spin_polarised = self.parameters.spin_polarised
+                calc_dos.pseudo_dir = calc_bands.parameters.pseudo_dir
+                self.run_calculator(calc_dos)
 
-        # Plot the band structure and DOS
-        self.plot_bandstructure(bs, dos)
+                # Prepare the DOS for plotting
+                dos = calc_dos.results['dos']
+                dos._energies -= vbe
+            else:
+                # Skip if the pseudos don't have the requisite PP_PSWFC blocks
+                utils.warn('Some of the pseudopotentials do not have PP_PSWFC blocks, which means a projected DOS '
+                           'calculation is not possible. Skipping...')
+                dos = None
+
+            # Plot the band structure and DOS
+            self.plot_bandstructure(bs, dos)
