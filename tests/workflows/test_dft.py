6696e90c1cf89fd94c1719fada85bd75060ba3c9
Edward Linscott <edwardlinscott@gmail.com>
2022-05-20 17:32:18 +0200

    Added pwbandstructure test

---
+++ tests/workflows/test_dft.py
@@ -0,0 +1,10 @@
+import pytest
+from koopmans import workflows, utils
+
+
+def test_pwbandstructure_si(silicon, mockable, tmp_path, sys2file):
+    with utils.chdir(tmp_path):
+        wf = workflows.PWBandStructureWorkflow(
+            parameters={'pseudo_library': 'pseudo_dojo_standard', 'base_functional': 'pbesol', 'from_scratch': True},
+            name='si', **silicon)
+        wf.run()
