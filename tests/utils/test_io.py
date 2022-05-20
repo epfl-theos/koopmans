36d1e153d341436dda7e31c568f3372409f3b451
Edward Linscott <edwardlinscott@gmail.com>
2022-05-20 12:30:36 +0200

    Added test for reading & writing of u matrices

--- tests/utils/test_io.py
+++ tests/utils/test_io.py
@@ -29,3 +29,26 @@ def test_write_read_wannier_hr_file(tmp_path):
         assert np.allclose(rvec_out, rvec_in)
         assert weights_out == weights_in
         assert num_wann_out == len(rvec_in)
+
+
+def test_write_read_wannier_u_file(tmp_path):
+    '''
+    Test function for utils.write_wannier_u_file and utils.read_wannier_u_file
+    '''
+
+    with utils.chdir(tmp_path):
+        # Dummy data
+        kpts = np.array([[0, 0, 0], [0.5, 0, 0]])
+        n_k = len(kpts)
+        n_w = 4
+        umat = np.arange(n_k * n_w**2, dtype=complex).reshape((n_k, n_w, n_w))
+
+        # Write u file
+        utils.write_wannier_u_file('test_u.mat', umat, kpts)
+
+        # Read u file
+        umat_out, kpts_out, _ = utils.read_wannier_u_file('test_u.mat')
+
+        # Compare the two
+        assert np.allclose(umat, umat_out)
+        assert np.allclose(kpts, kpts_out)
