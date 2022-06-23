c53ad98931ba375167a478c2b363e576c53d8e89
Edward Linscott <edwardlinscott@gmail.com>
2022-06-23 18:02:15 +0200

    Minor bugfix to qei_to_json tests that were writing out to the datadir instead of the tmpdir

--- tests/test_qei_to_json.py
+++ tests/test_qei_to_json.py
@@ -6,10 +6,10 @@ from koopmans.qei_to_json import qei_to_json
 def test_pwi_to_json(tmpdir, datadir):
     with utils.chdir(tmpdir):
         basedir = datadir / 'qei_to_json'
-        qei_to_json(basedir / 'example.pwi', basedir / 'example.json')
+        qei_to_json(basedir / 'example.pwi', 'example.json')
 
 
 def test_cpi_to_json(tmpdir, datadir):
     with utils.chdir(tmpdir):
         basedir = datadir / 'qei_to_json'
-        qei_to_json(basedir / 'example.cpi', basedir / 'example.json')
+        qei_to_json(basedir / 'example.cpi', 'example.json')
