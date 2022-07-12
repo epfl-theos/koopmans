8745f461120f20624af821b87f13630d847767f1
Edward Linscott <edwardlinscott@gmail.com>
2022-07-12 11:34:14 +0200

    Fixed bug for heavy atoms (#159)
    Currently for the pDOS calculations we want to identify which subshells each pDOS corresponds to.
    
    Unfortunately, pseudopotential files do not universally contain this information. Instead, we _guess_ what subshells might be in the valence based on their `z_valence` and their total Z.
    
    The old implementation did not support sufficiently large values of Z. This PR makes the guessing of expected subshells more robust. We also switch over to using `upf_to_json` rather than an xml parser because some pseudos aren't xml-compliant.

--- koopmans/workflows/_wannierize.py
+++ koopmans/workflows/_wannierize.py
@@ -196,7 +196,7 @@ class WannierizeWorkflow(Workflow):
             # Calculate a projected DOS
             pseudos = [read_pseudo_file(calc_pw_bands.parameters.pseudo_dir / p) for p in
                        self.pseudopotentials.values()]
-            if all([int(p.find('PP_HEADER').get('number_of_wfc')) > 0 for p in pseudos]):
+            if all([p['header']['number_of_wfc'] > 0 for p in pseudos]):
                 calc_dos = self.new_calculator('projwfc', filpdos=self.name)
                 calc_dos.directory = 'pdos'
                 calc_dos.pseudopotentials = self.pseudopotentials
