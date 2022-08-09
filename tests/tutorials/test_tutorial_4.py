71031e918edb3367051306e48a41586824bd48a2
Edward Linscott <edwardlinscott@gmail.com>
2022-08-10 00:18:59 +0200

    Generated benchmarks for tutorial 3 once again, now making sure there are no two tests that run a calculation in the same location

---
+++ tests/tutorials/test_tutorial_4.py
@@ -0,0 +1,22 @@
+from pathlib import Path
+
+import pytest
+
+from koopmans import base_directory
+from koopmans.io import read
+from koopmans.utils import chdir
+
+tutorial_dir = base_directory / 'tutorials' / 'tutorial_4'
+
+
+@pytest.mark.tutorials
+def test_run(tutorial_patch):
+    with chdir(tutorial_dir):
+        wf = read('h2o_conv.json')
+        wf.run()
+
+
+@pytest.mark.tutorials
+def test_plot():
+    with chdir(tutorial_dir):
+        exec(open('plot.py', 'r').read())
