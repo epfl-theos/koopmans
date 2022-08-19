3ed01b7582ebce7c1256ac7948d4cc75c8d6889d
Edward Linscott <edwardlinscott@gmail.com>
2022-08-19 10:28:40 +0200

    Updated version to include 1 after beta

--- koopmans/__init__.py
+++ koopmans/__init__.py
@@ -1,5 +1,5 @@
 'Python module for running KI and KIPZ calculations with Quantum Espresso'
 from pathlib import Path
 
-__version__ = '1.0.0-beta'
+__version__ = '1.0.0-beta.1'
 base_directory = Path(__path__[0]).parent
