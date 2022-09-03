03f85008bd900a91302999c7982952d0ed5511a2
Yannick Schubert <schuyann@student.ethz.ch>
2022-09-03 09:36:50 +0200

    perturbed positions for tutorial

--- tutorials/tutorial_5/perturb_positions.py
+++ tutorials/tutorial_5/perturb_positions.py
@@ -1,6 +1,21 @@
 import numpy as np
+import copy
+from ase import io
 
-from ase import Atoms, io
+np.random.seed(0)
+cell = np.diag([6.892900, 6.892900, 6.892900])
 
-snapshots = io.read("h2o_unpertubed_positions.xyz", index='0')
-print(snapshots.positions)
+unperturbed_h2o = io.read("h2o_unpertubed_positions.xyz", index='0')
+unperturbed_h2o.set_cell(cell)
+
+
+trajectory = []
+
+for i in range(10):
+    random_pertubation = np.random.rand(3, 3)-0.5
+    new_perturbed_h2o = copy.deepcopy(unperturbed_h2o)
+    new_perturbed_h2o.positions += 0.5*random_pertubation
+    trajectory.append(new_perturbed_h2o)
+
+io.write('snapshots.gif', trajectory, interval=500, rotation='-100y,-10x')
+io.write('snapshots.xyz', trajectory)
