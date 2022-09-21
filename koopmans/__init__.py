97fe5be5ac45760e7cb7716f54e88d173b69bab2
Edward Linscott <edwardlinscott@gmail.com>
2022-09-21 14:50:55 +0200

    Updated koopmans-qe-utils

--- koopmans/__init__.py
+++ koopmans/__init__.py
@@ -1,5 +1,5 @@
-'Python module for running KI and KIPZ calculations with Quantum Espresso'
+'koopmans: a package for performing and automating Koopmans functional calculations'
 from pathlib import Path
 
-__version__ = '1.0.0-beta.2'
+__version__ = '1.0.0-beta.3'
 base_directory = Path(__path__[0]).parent
