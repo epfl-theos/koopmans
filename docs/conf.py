3a1c13d50e0f33eda5f8ddb9470b8a1448a4016c
Edward Linscott <edwardlinscott@gmail.com>
2022-09-27 18:32:48 +0200

    Adding erroneous import in conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -12,8 +12,6 @@ import sys
 import sphinx_rtd_theme
 from sphinx_pyproject import SphinxConfig
 
-import koopmans
-
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
