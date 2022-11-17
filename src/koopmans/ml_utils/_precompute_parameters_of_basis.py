17c79371d5102d8cb140c4dfd67147e4c6aa9175
Edward Linscott <edwardlinscott@gmail.com>
2022-11-17 15:30:12 +0100

    Fix typo Hilmanen -> Himanen

--- src/koopmans/ml_utils/_precompute_parameters_of_basis.py
+++ src/koopmans/ml_utils/_precompute_parameters_of_basis.py
@@ -34,7 +34,7 @@ def compute_overlap(n: int, n_prime: int, l: int, alphas: np.ndarray):
 
 def compute_s(n_max: int, l: int, alphas: np.ndarray) -> np.ndarray:
     """
-    Computes the overlap matrix S (as in eq. (26) in Hilmanen et al 2020).
+    Computes the overlap matrix S (as in eq. (26) in Himanen et al 2020).
     """
 
     s = np.zeros((n_max, n_max))
@@ -56,7 +56,7 @@ def lowdin(s: np.ndarray):
 
 def compute_beta(n_max: int, l: int, alphas: np.ndarray) -> np.ndarray:
     """
-    Computes beta such that the corresponding basis is orthogonal (as in eq. (25) Hilmanen et al 2020).
+    Computes beta such that the corresponding basis is orthogonal (as in eq. (25) Himanen et al 2020).
     """
 
     s = compute_s(n_max, l, alphas)
@@ -67,7 +67,7 @@ def compute_beta(n_max: int, l: int, alphas: np.ndarray) -> np.ndarray:
 
 def precompute_parameters_of_radial_basis(n_max: int, l_max: int, r_min_thr: float, r_max_thr: float, dirs: Optional[Dict[str, Path]] = None):
     """
-    Precomputes the alphas and betas needed to define the basis functions (as in Hilmanen et al 2020).
+    Precomputes the alphas and betas needed to define the basis functions (as in Himanen et al 2020).
     """
 
     thr = 10**(-3)
@@ -100,26 +100,3 @@ def precompute_parameters_of_radial_basis(n_max: int, l_max: int, r_min_thr: flo
                                                           for x in [n_max, l_max, r_min_thr, r_max_thr]) + '.dat'))
         alphas.tofile(dirs['alphas'] / ('alphas_' + '_'.join(str(x)
                                                              for x in [n_max, l_max, r_min_thr, r_max_thr]) + '.dat'))
-
-    # r = np.linspace(0, 4, 1000)
-    # for l in range(l_max):
-    #     plt.figure()
-    #     for n in range(n_max):
-    #         def f(r): return gb.phi(r, l, alphas[n, l])
-    #         plt.plot(r, f(r), label="n = " + str(n+1))
-    #     if(l == 0):
-    #         plt.vlines(r_thrs, ymin=0.0, ymax=1.0)
-    #     plt.plot(r, thr*np.ones(len(r)), label="threshold value")
-    #     plt.legend()
-    #     plt.savefig(curr_folder + '/plots/phi_n' + str(l) + '.png')
-
-    # r = np.linspace(0, 3, 1000)
-    # for l in range(l_max):
-    #     plt.figure()
-    #     for n in range(n_max):
-    #         def f(r): return gb.g(r, n, n_max, l, betas, alphas)
-    #         plt.plot(r, f(r), label="n = " + str(n+1))
-    #     plt.legend()
-    #     plt.savefig(curr_folder + '/plots/g_n' + str(l) + '.png')
-
-    # return alphas, betas
