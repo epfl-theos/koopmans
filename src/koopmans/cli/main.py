240816c11840873c5e9060a4c368edab6141c404
Edward Linscott <edwardlinscott@gmail.com>
2022-11-25 17:43:52 +0100

    Disabled traceback and allowed it to be reenabled with the --traceback flag

--- src/koopmans/cli/main.py
+++ src/koopmans/cli/main.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 
+import sys
 import argparse
 import textwrap
 
@@ -16,12 +17,18 @@ def main():
         epilog='See https://koopmans-functionals.org for more details')
     parser.add_argument('json', metavar='system.json', type=str,
                         help='a single JSON file containing the workflow and code settings')
+    parser.add_argument('-t', '--traceback', action='store_true', help='enable traceback')
 
     # Parse arguments
     args = parser.parse_args()
 
     # Reading in JSON file
     workflow = read(args.json)
+
+    # Set traceback behaviour
+    if not args.traceback:
+        sys.tracebacklimit = 0
+    
 
     # Run workflow
     workflow.run()
