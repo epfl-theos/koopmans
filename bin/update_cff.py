50e77cb768d7f19fd6fd1ff4dd4dae707fcd3002
Edward Linscott <edwardlinscott@gmail.com>
2023-02-22 15:59:27 +0100

    Add a CITATION.cff file (#197)
    This PR creates a `CITATION.cff` file that allows people to download a reference for the code directly from GitHub.
    
    It also creates a workflow to ensure that the version of the code listed in this file is kept up-to-date.

---
+++ bin/update_cff.py
@@ -0,0 +1,18 @@
+import sys
+import yaml
+if sys.version_info >= (3, 8):
+    from importlib import metadata
+else:
+    import importlib_metadata as metadata
+
+
+# Load the CITATION.cff file
+with open('CITATION.cff', 'r') as fd:
+    data = yaml.load(fd, Loader=yaml.Loader)
+
+# Update version
+data['version'] = metadata.version('koopmans')
+
+# Overwrite the CITATION.cff file
+with open('CITATION.cff', 'w') as fd:
+    yaml.dump(data, fd, allow_unicode=True)
