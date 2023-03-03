8b30f8f78becf538d5ad7532a259f9c73c858298
Edward Linscott <edwardlinscott@gmail.com>
2023-03-03 12:24:38 +0100

    Add special_points check to kpoints tests

--- tests/test_kpoints.py
+++ tests/test_kpoints.py
@@ -12,6 +12,7 @@ from tests.helpers import strategies as kst
 def test_roundtrip_kpath_dict(bp: BandPath):
     # TODO work out why bp.special_points changes
     bp_roundtrip = dict_to_kpath(kpath_to_dict(bp))
+    assert bp.special_points.keys() == bp_roundtrip.special_points.keys()
     assert np.allclose(bp_roundtrip.cell, bp.cell)
     assert bp_roundtrip.path == bp.path
     assert len(bp.kpts) == len(bp_roundtrip.kpts)
@@ -22,6 +23,7 @@ def test_roundtrip_kpath_dict(bp: BandPath):
 def test_roundtrip_Kpoints_dict(kpts):
     # TODO work out why kpts.path.special_points changes
     kpts_roundtrip = Kpoints.fromdict(kpts.todict())
+    assert kpts.path.special_points.keys() == kpts_roundtrip.path.special_points.keys()
     assert kpts.gamma_only == kpts_roundtrip.gamma_only
     assert kpts.grid == kpts_roundtrip.grid
     assert np.allclose(kpts.path.cell, kpts_roundtrip.path.cell)
