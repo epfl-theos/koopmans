d1207eae72d8d71bed457fbd8e19d294729ff327
Edward Linscott <edwardlinscott@gmail.com>
2023-12-14 08:19:14 +0100

    Adding external projector keywords

--- src/koopmans/settings/_pw2wannier.py
+++ src/koopmans/settings/_pw2wannier.py
@@ -6,7 +6,7 @@ class PW2WannierSettingsDict(SettingsDict):
         super().__init__(valid=['outdir', 'prefix', 'seedname', 'write_mmn',
                                 'write_amn', 'write_uHu', 'uHu_formatted',
                                 'write_unk', 'reduce_unk', 'wan_mode', 'spin_component',
-                                'atom_proj'],
+                                'atom_proj', 'atom_proj_ext', 'atom_proj_dir'],
                          defaults={'outdir': 'TMP', 'prefix': 'kc', 'seedname': 'wann',
                                    'wan_mode': 'standalone'},
                          are_paths=['outdir'],
