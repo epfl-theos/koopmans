343bec14ea977150c5d8c95bd2ac5d4453d543a5
Julian Geiger <julian.geiger@psi.ch>
2024-05-01 07:58:00 +0200

    Moved `from_kcwscreen_to_KcwCalculation` out

--- src/koopmans/calculators/_koopmans_screen.py
+++ src/koopmans/calculators/_koopmans_screen.py
@@ -9,6 +9,7 @@ Written by Edward Linscott Feb 2021
 import os
 
 import numpy as np
+from aiida_koopmans.helpers import from_kcwscreen_to_KcwCalculation
 from ase import Atoms
 from ase.calculators.espresso import KoopmansScreen
 
@@ -17,88 +18,6 @@ from koopmans.commands import ParallelCommandWithPostfix
 
 from ._utils import CalculatorABC, KCWannCalculator
 
-def from_kcwscreen_to_KcwCalculation(kcw_calculator):
-    
-    """
-    The input parent folder is meant to be set later, at least for now.
-    """
-    
-    from aiida_koopmans.calculations.kcw import KcwCalculation
-    from aiida import orm, load_profile
-    load_profile()
-    
-    builder = KcwCalculation.get_builder()
-    
-    control_namelist = [
-                    'kcw_iverbosity',
-                    'kcw_at_ks'      ,
-                    'calculation'    ,
-                    'lrpa'           ,
-                    'mp1'            ,
-                    'mp2'            ,
-                    'mp3'            ,
-                    'homo_only'      ,
-                    'read_unitary_matrix' ,
-                    'l_vcut'         ,
-                    'spin_component' ,
-                    ]
-    
-    control_dict = {k:v if k in control_namelist else None for k,v in kcw_calculator.parameters.items()}
-    control_dict['calculation'] = "screen"
-        
-    for k in list(control_dict):
-        if control_dict[k] is None:
-            control_dict.pop(k)
-            
-    if not any(kcw_calculator.atoms.pbc): control_dict['assume_isolated'] = "m-t"
-
-    wannier_namelist = [
-                    "check_ks"       ,
-                    "num_wann_occ"   ,
-                    "num_wann_emp"   ,
-                    "have_empty"     ,
-                    "has_disentangle",
-    ]
-
-    wannier_dict = {k:v if k in wannier_namelist else None for k,v in kcw_calculator.parameters.items()}
-
-    for k in list(wannier_dict):
-        if wannier_dict[k] is None:
-            wannier_dict.pop(k)
-    
-    screening_namelist = [
-                    'tr2'         , #: 1e-18,
-                    'nmix'        , #: 4,
-                    'niter'       , #: 33,
-                    'check_spread', #: True,
-    ]
-    
-    screening_dict = {k:v if k in screening_namelist else None for k,v in kcw_calculator.parameters.items()}
-    
-    for k in list(screening_dict):
-        if screening_dict[k] is None:
-            screening_dict.pop(k)
-
-    kcw_screen_params = {
-            "CONTROL":control_dict,
-            "WANNIER":wannier_dict,
-            "SCREEN":screening_dict,
-        }
-    
-    builder.parameters = orm.Dict(kcw_screen_params)
-    builder.code = orm.load_code(kcw_calculator.mode["kcw_code"])
-    builder.metadata = kcw_calculator.mode["metadata"]
-    if "metadata_kcw" in kcw_calculator.mode: builder.metadata = kcw_calculator.mode["metadata_kcw"]
-    builder.parent_folder = kcw_calculator.parent_folder
-    
-    if hasattr(kcw_calculator, "wannier90_files") and control_dict.get("read_unitary_matrix",False):
-        builder.wann_u_mat = kcw_calculator.wannier90_files["occ"]["u_mat"]
-        builder.wann_emp_u_mat = kcw_calculator.wannier90_files["emp"]["u_mat"]
-        builder.wann_emp_u_dis_mat = kcw_calculator.wannier90_files["emp"]["u_dis_mat"]
-        builder.wann_centres_xyz = kcw_calculator.wannier90_files["occ"]["centres_xyz"]
-        builder.wann_emp_centres_xyz = kcw_calculator.wannier90_files["emp"]["centres_xyz"]
-
-    return builder
 
 class KoopmansScreenCalculator(KCWannCalculator, KoopmansScreen, CalculatorABC):
     # Subclass of KCWannCalculator for calculating screening parameters with kcw.x
@@ -154,8 +73,8 @@ class KoopmansScreenCalculator(KCWannCalculator, KoopmansScreen, CalculatorABC):
         if self.mode == "ase":
             return super().calculate()
         else: # MB mod
-            builder = from_kcwscreen_to_KcwCalculation(self)
-            from aiida.engine import run_get_node,submit
+            builder = from_kcwscreen_to_KcwCalculation(kcw_calculator=self)
+            from aiida.engine import run_get_node, submit
             running = run_get_node(builder)
             
             # once the running if completed
