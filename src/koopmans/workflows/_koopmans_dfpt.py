0198aa25b16323c76ce09a5e09cd4296c74192de
nscolonna <nicola.colonna@psi.ch>
2024-06-19 16:46:31 +0200

    Implementation of the logic for nspin=2 calculation with the DFPT worflow. wann2kc and ham seems to be working

--- src/koopmans/workflows/_koopmans_dfpt.py
+++ src/koopmans/workflows/_koopmans_dfpt.py
@@ -51,7 +51,8 @@ class KoopmansDFPTWorkflow(Workflow):
                         if params.l_vcut is None:
                             params.l_vcut = True
                         if not params.l_vcut:
-                            raise ValueError('Gygi-Baldereschi corrections require l_vcut = True')
+                            raise ValueError(
+                                'Gygi-Baldereschi corrections require l_vcut = True')
 
                 # Makov-Payne
                 if self.parameters.mp_correction:
@@ -70,11 +71,13 @@ class KoopmansDFPTWorkflow(Workflow):
                     if params.l_vcut is None:
                         params.l_vcut = False
                     if params.l_vcut:
-                        raise ValueError('Aperiodic systems require l_vcut = False')
+                        raise ValueError(
+                            'Aperiodic systems require l_vcut = False')
 
                 # Makov-Payne (not used for aperiodic systems)
                 if getattr(params, 'eps_inf', None) not in [None, 1.0]:
-                    raise ValueError('Aperiodic systems should not have a non-zero eps_inf')
+                    raise ValueError(
+                        'Aperiodic systems should not have a non-zero eps_inf')
 
                 # Martyna-Tuckerman
                 if self.parameters.mt_correction:
@@ -86,24 +89,55 @@ class KoopmansDFPTWorkflow(Workflow):
                                 f'assume_isolated = {params.assume_isolated} is incompatible with mt_correction = True')
 
         # Initialize the bands
-        nocc = pseudopotentials.nelec_from_pseudos(
-            self.atoms, self.pseudopotentials, self.parameters.pseudo_directory) // 2
-        if all(self.atoms.pbc):
-            exclude_bands = self.calculator_parameters['w90'].get('exclude_bands', [])
-            nocc -= len(exclude_bands)
-            ntot = self.projections.num_wann()
+        if self.parameters.spin_polarized:
+            nelec = pseudopotentials.nelec_from_pseudos(
+                self.atoms, self.pseudopotentials, self.parameters.pseudo_directory)
+            tot_mag = self.calculator_parameters['pw'].tot_magnetization
+            nocc_up = (nelec + tot_mag) // 2
+            nocc_dw = (nelec - tot_mag) // 2
+            if all(self.atoms.pbc):
+                # Using Wannier functions
+                ntot_up = self.projections.num_wann(spin='up')
+                ntot_dw = self.projections.num_wann(spin='down')
+            else:
+                # Using KS bands
+                ntot_up = self.calculator_parameters['pw'].nbnd
+                ntot_dw = self.calculator_parameters['pw'].nbnd
+            nemp_up = ntot_up - nocc_up
+            nemp_dw = ntot_dw - nocc_dw
+            filling = [[True for _ in range(nocc_up)] + [False for _ in range(nemp_up)],
+                       [True for _ in range(nocc_dw)] + [False for _ in range(nemp_dw)]]
+            if self.parameters.orbital_groups is None:
+                self.parameters.orbital_groups = [
+                    list(range(nocc_up + nemp_up)), list(range(nocc_dw + nemp_dw))]
+            tols: Dict[str, float] = {}
+            for key in ['self_hartree', 'spread']:
+                val = self.parameters.get(f'orbital_groups_{key}_tol', None)
+                if val is not None:
+                    tols[key] = val
+            self.bands = Bands(n_bands=[len(f) for f in filling], n_spin=2, spin_polarized=self.parameters.spin_polarized,
+                               filling=filling, groups=self.parameters.orbital_groups, tolerances=tols)
         else:
-            ntot = self.calculator_parameters['pw'].nbnd
-        nemp = ntot - nocc
-        if self.parameters.orbital_groups is None:
-            self.parameters.orbital_groups = [list(range(nocc + nemp))]
-        tols: Dict[str, float] = {}
-        for key in ['self_hartree', 'spread']:
-            val = self.parameters.get(f'orbital_groups_{key}_tol', None)
-            if val is not None:
-                tols[key] = val
-        self.bands = Bands(n_bands=nocc + nemp, filling=[[True] * nocc + [False] * nemp],
-                           groups=self.parameters.orbital_groups, tolerances=tols)
+            nocc = pseudopotentials.nelec_from_pseudos(
+                self.atoms, self.pseudopotentials, self.parameters.pseudo_directory) // 2
+            if all(self.atoms.pbc):
+                exclude_bands = self.calculator_parameters['w90'].get(
+                    'exclude_bands', [])
+                nocc -= len(exclude_bands)
+                ntot = self.projections.num_wann()
+            else:
+                ntot = self.calculator_parameters['pw'].nbnd
+            nemp = ntot - nocc
+            filling = [[True] * nocc + [False] * nemp]
+            if self.parameters.orbital_groups is None:
+                self.parameters.orbital_groups = [list(range(nocc + nemp))]
+            tols: Dict[str, float] = {}
+            for key in ['self_hartree', 'spread']:
+                val = self.parameters.get(f'orbital_groups_{key}_tol', None)
+                if val is not None:
+                    tols[key] = val
+            self.bands = Bands(n_bands=nocc + nemp, filling=filling,
+                               groups=self.parameters.orbital_groups, tolerances=tols)
 
         # Populating kpoints if absent
         if not all(self.atoms.pbc):
@@ -131,7 +165,8 @@ class KoopmansDFPTWorkflow(Workflow):
 
         if self.parameters.dfpt_coarse_grid is not None:
             self.print('Coarse grid calculations', style='heading')
-            coarse_wf = self.__class__.fromparent(self, scf_kgrid=self.kpoints.grid)
+            coarse_wf = self.__class__.fromparent(
+                self, scf_kgrid=self.kpoints.grid)
             coarse_wf.parameters.dfpt_coarse_grid = None
             coarse_wf.kpoints.grid = self.parameters.dfpt_coarse_grid
             coarse_wf._perform_ham_calc = False
@@ -144,12 +179,14 @@ class KoopmansDFPTWorkflow(Workflow):
                 if key.startswith('w90'):
                     self.calculator_parameters[key].write_u_matrices = True
                     self.calculator_parameters[key].write_xyz = True
-            wf_workflow = WannierizeWorkflow.fromparent(self, force_nspin2=True, scf_kgrid=self._scf_kgrid)
+            wf_workflow = WannierizeWorkflow.fromparent(
+                self, force_nspin2=True, scf_kgrid=self._scf_kgrid)
             wf_workflow.run()
 
         else:
             # Run PW
-            self.print('Initialization of density and variational orbitals', style='heading')
+            self.print(
+                'Initialization of density and variational orbitals', style='heading')
 
             # Create the workflow
             pw_workflow = DFTPWWorkflow.fromparent(self)
@@ -166,7 +203,8 @@ class KoopmansDFPTWorkflow(Workflow):
         # Copy the outdir to the base directory
         base_outdir = self.calculator_parameters['pw'].outdir
         base_outdir.mkdir(exist_ok=True)
-        scf_calcs = [c for c in self.calculations if isinstance(c, PWCalculator) and c.parameters.calculation == 'scf']
+        scf_calcs = [c for c in self.calculations if isinstance(
+            c, PWCalculator) and c.parameters.calculation == 'scf']
         init_outdir = scf_calcs[-1].parameters.outdir
         if self.parameters.from_scratch and init_outdir != base_outdir:
             utils.symlink(f'{init_outdir}/*', base_outdir)
@@ -177,7 +215,8 @@ class KoopmansDFPTWorkflow(Workflow):
 
             # Convert from wannier to KC
             self.print('Conversion to Koopmans format', style='subheading')
-            wann2kc_calc = self.new_calculator('wann2kc', spin_component=spin_component)
+            wann2kc_calc = self.new_calculator(
+                'wann2kc', spin_component=spin_component)
             if self.parameters.spin_polarized:
                 wann2kc_calc.directory /= f'spin_{spin_component}'
             self.run_calculator(wann2kc_calc)
@@ -185,15 +224,18 @@ class KoopmansDFPTWorkflow(Workflow):
             # Calculate screening parameters
             if self.parameters.calculate_alpha:
                 if self.parameters.dfpt_coarse_grid is None:
-                    self.print('Calculation of screening parameters', style='heading')
+                    self.print('Calculation of screening parameters',
+                               style='heading')
 
                     # Group the bands by spread
-                    self.bands.assign_groups(sort_by='spread', allow_reassignment=True)
+                    self.bands.assign_groups(
+                        sort_by='spread', allow_reassignment=True)
 
                     if len(self.bands.to_solve) == len(self.bands):
                         # If there is no orbital grouping, do all orbitals in one calculation
                         # 1) Create the calculator
-                        kc_screen_calc = self.new_calculator('kc_screen', spin_component=spin_component)
+                        kc_screen_calc = self.new_calculator(
+                            'kc_screen', spin_component=spin_component)
                         if self.parameters.spin_polarized:
                             kc_screen_calc.directory /= f'spin_{spin_component}'
 
@@ -232,13 +274,15 @@ class KoopmansDFPTWorkflow(Workflow):
             # Calculate the Hamiltonian
             if self._perform_ham_calc:
                 self.print('Construction of the Hamiltonian', style='heading')
-                kc_ham_calc = self.new_calculator('kc_ham', kpts=self.kpoints.path, spin_component=spin_component)
+                kc_ham_calc = self.new_calculator(
+                    'kc_ham', kpts=self.kpoints.path, spin_component=spin_component)
                 if self.parameters.spin_polarized:
                     kc_ham_calc.directory /= f'spin_{spin_component}'
 
                 if self.parameters.calculate_alpha and self.parameters.dfpt_coarse_grid is None:
                     if kc_ham_calc.parameters.lrpa != kc_screen_calc.parameters.lrpa:
-                        raise ValueError('Do not set "lrpa" to different values in the "screen" and "ham" blocks')
+                        raise ValueError(
+                            'Do not set "lrpa" to different values in the "screen" and "ham" blocks')
                 self.run_calculator(kc_ham_calc)
 
                 # Postprocessing
@@ -248,7 +292,8 @@ class KoopmansDFPTWorkflow(Workflow):
                     self.print(f'\nPostprocessing', style='heading')
                     ui_workflow = UnfoldAndInterpolateWorkflow.fromparent(self)
                     if self.parameters.spin_polarized:
-                        ui_workflow.run(subdirectory=f'postproc/spin_{spin_component}')
+                        ui_workflow.run(
+                            subdirectory=f'postproc/spin_{spin_component}')
                     else:
                         ui_workflow.run(subdirectory='postproc')
 
@@ -260,8 +305,10 @@ class KoopmansDFPTWorkflow(Workflow):
             return
 
         # Identify the relevant calculators
-        wann2kc_calc = [c for c in self.calculations if isinstance(c, Wann2KCCalculator)][-1]
-        kc_ham_calc = [c for c in self.calculations if isinstance(c, KoopmansHamCalculator)][-1]
+        wann2kc_calc = [c for c in self.calculations if isinstance(
+            c, Wann2KCCalculator)][-1]
+        kc_ham_calc = [c for c in self.calculations if isinstance(
+            c, KoopmansHamCalculator)][-1]
 
         # Plot the bandstructure if the band path has been specified
         bs = kc_ham_calc.results['band structure']
@@ -283,6 +330,9 @@ class KoopmansDFPTWorkflow(Workflow):
         calc.parameters.kcw_at_ks = not all(self.atoms.pbc)
         calc.parameters.read_unitary_matrix = all(self.atoms.pbc)
 
+        for k, v in kwargs.items():
+            setattr(calc.parameters, k, v)
+
         if calc_presets == 'wann2kc':
             if all(self.atoms.pbc):
                 calc.directory = 'wannier'
@@ -297,11 +347,12 @@ class KoopmansDFPTWorkflow(Workflow):
         else:
             calc.directory = 'hamiltonian'
             calc.parameters.do_bands = all(self.atoms.pbc)
-            calc.alphas = self.bands.alphas
+            if not self.parameters.spin_polarized and len(self.bands.alphas) != 1:
+                raise ValueError(
+                    'The list of screening parameters should be length 1 for spin-unpolarized calculations')
+            calc.alphas = self.bands.alphas[calc.parameters.spin_component - 1]
 
         if all(self.atoms.pbc):
-            import ipdb
-            ipdb.set_trace()
             if self.parameters.spin_polarized:
                 if calc.parameters.spin_component == 1:
                     ntot = self.projections.num_wann(spin='up')
@@ -319,7 +370,7 @@ class KoopmansDFPTWorkflow(Workflow):
                 nemp_pw = self.calculator_parameters['pw'].nbnd - nocc
 
             have_empty = (nemp > 0)
-            #has_disentangle = (self.projections.num_bands() != nocc + nemp)
+            # has_disentangle = (self.projections.num_bands() != nocc + nemp)
             has_disentangle = (nemp != nemp_pw)
         else:
             if self.parameters.spin_polarized:
@@ -339,7 +390,7 @@ class KoopmansDFPTWorkflow(Workflow):
         calc.parameters.have_empty = have_empty
         calc.parameters.has_disentangle = has_disentangle
 
-        # Apply any additional calculator keywords passed as kwargs
+        # Ensure that any additional calculator keywords passed as kwargs are still that value
         for k, v in kwargs.items():
             setattr(calc.parameters, k, v)
 
@@ -349,8 +400,6 @@ class KoopmansDFPTWorkflow(Workflow):
         # Create this (possibly nested) directory
         calc.directory.mkdir(parents=True, exist_ok=True)
 
-        #import ipdb
-        # ipdb.set_trace()
         # Provide the rotation matrices and the wannier centers
         if all(self.atoms.pbc):
             if self.parameters.spin_polarized:
@@ -360,12 +409,15 @@ class KoopmansDFPTWorkflow(Workflow):
                     dir_suffix = '_down'
             else:
                 dir_suffix = ''
-            utils.symlink(f'wannier/occ{dir_suffix}/wann_u.mat', f'{calc.directory}/', exist_ok=True)
-            utils.symlink(f'wannier/emp{dir_suffix}/wann_u.mat', f'{calc.directory}/wann_emp_u.mat', exist_ok=True)
+            utils.symlink(
+                f'wannier/occ{dir_suffix}/wann_u.mat', f'{calc.directory}/', exist_ok=True)
+            utils.symlink(f'wannier/emp{dir_suffix}/wann_u.mat',
+                          f'{calc.directory}/wann_emp_u.mat', exist_ok=True)
             if Path(f'wannier/emp{dir_suffix}/wann_u_dis.mat').exists():
                 utils.symlink(f'wannier/emp/wann_u_dis.mat',
                               f'{calc.directory}/wann_emp_u_dis.mat', exist_ok=True)
-            utils.symlink(f'wannier/occ{dir_suffix}/wann_centres.xyz', f'{calc.directory}/', exist_ok=True)
+            utils.symlink(
+                f'wannier/occ{dir_suffix}/wann_centres.xyz', f'{calc.directory}/', exist_ok=True)
             utils.symlink(f'wannier/emp{dir_suffix}/wann_centres.xyz',
                           f'{calc.directory}/wann_emp_centres.xyz', exist_ok=True)
 
