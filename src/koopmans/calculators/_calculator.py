a88c8af2d04b02c95ae6f9b101e6d92758972b50
Edward Linscott <edwardlinscott@gmail.com>
2024-09-11 13:54:51 +0200

    Removd rogue import of old ml plotting routines

--- src/koopmans/calculators/_calculator.py
+++ src/koopmans/calculators/_calculator.py
@@ -281,7 +281,8 @@ class CalculatorExt():
             setattr(calc, k.lstrip('_'), v)
         return calc
 
-    def link_file(self, src_calc: utils.HasDirectoryAttr | None, src_filename: Path, dest_filename: Path, symlink: bool = False, recursive_symlink: bool = False, overwrite: bool = False):
+    def link_file(self, src_calc: utils.HasDirectoryAttr | None, src_filename: Path, dest_filename: Path, symlink: bool = False,
+                  recursive_symlink: bool = False, overwrite: bool = False):
         if src_filename.is_absolute() and src_calc is not None:
             raise ValueError(f'`src_filename` in `{self.__class__.__name__}.link_file()` must be a relative path if a '
                              f'`src_calc` is provided')
