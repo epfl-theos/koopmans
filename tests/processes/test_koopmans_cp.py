15d0153633e3ad127456c2b25aa3a1eafd73f721
Edward Linscott <edwardlinscott@gmail.com>
2024-10-04 10:47:01 +0200

    Make sure lists of input files to koopmans_cp process tests are sorted

--- tests/processes/test_koopmans_cp.py
+++ tests/processes/test_koopmans_cp.py
@@ -11,9 +11,9 @@ from koopmans.processes.koopmans_cp import (ConvertFilesFromSpin1To2,
 
 def test_convert_files_from_spin_1_to_2_process(tmpdir, check_patch, datadir):
     with utils.chdir(tmpdir):
-        spin_1_files = [AbsoluteFilePointer(f) for f in (datadir / 'kcp' / 'spin_1_files').glob('*')]
-        spin_2_up_files = (datadir / 'kcp' / 'spin_2_files').glob('*1.???')
-        spin_2_down_files = (datadir / 'kcp' / 'spin_2_files').glob('*2.???')
+        spin_1_files = sorted([AbsoluteFilePointer(f) for f in (datadir / 'kcp' / 'spin_1_files').glob('*')])
+        spin_2_up_files = sorted((datadir / 'kcp' / 'spin_2_files').glob('*1.???'))
+        spin_2_down_files = sorted((datadir / 'kcp' / 'spin_2_files').glob('*2.???'))
 
         process = ConvertFilesFromSpin1To2(spin_1_files=spin_1_files,
                                            spin_2_up_files=spin_2_up_files,
@@ -24,8 +24,8 @@ def test_convert_files_from_spin_1_to_2_process(tmpdir, check_patch, datadir):
 
 def test_convert_files_from_spin_2_to_1_process(tmpdir, check_patch, datadir):
     with utils.chdir(tmpdir):
-        spin_1_files = (datadir / 'kcp' / 'spin_1_files').glob('*')
-        spin_2_files = [AbsoluteFilePointer(f) for f in (datadir / 'kcp' / 'spin_2_files').glob('*1.???')]
+        spin_1_files = sorted((datadir / 'kcp' / 'spin_1_files').glob('*'))
+        spin_2_files = sorted([AbsoluteFilePointer(f) for f in (datadir / 'kcp' / 'spin_2_files').glob('*1.???')])
 
         process = ConvertFilesFromSpin2To1(spin_2_files=spin_2_files,
                                            spin_1_files=spin_1_files)
