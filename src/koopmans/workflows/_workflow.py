17a4ce86d00da905d87e627c1b4430a4b5472247
Edward Linscott <edwardlinscott@gmail.com>
2024-10-09 17:08:32 +0200

    Added catch for when spglib symmetry check fails

--- src/koopmans/workflows/_workflow.py
+++ src/koopmans/workflows/_workflow.py
@@ -569,7 +569,7 @@ class Workflow(utils.HasDirectory, ABC):
 
             # Check symmetry of the system
             dataset = symmetrize.check_symmetry(self.atoms, 1e-6, verbose=False)
-            if dataset['number'] not in range(195, 231):
+            if dataset is None or dataset['number'] not in range(195, 231):
                 utils.warn('This system is not cubic and will therefore not have a uniform dielectric tensor. However, '
                            'the image-correction schemes that are currently implemented assume a uniform dielectric. '
                            'Proceed with caution')
